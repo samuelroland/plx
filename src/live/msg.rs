226d40d73771d3cfa298379eed99514af64d8eba
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2025-06-25 17:57:17 +0200

    feat(live): improve Msg enum with ForwardFile and ForwardResult from dedicated structs, add ClientNum wrapper type

--- src/live/msg.rs
+++ src/live/msg.rs
@@ -1,4 +1,7 @@
-use std::fmt::Display;
+use std::{
+    fmt::Display,
+    time::{Instant, SystemTime},
+};
 
 use serde::{Deserialize, Serialize};
 use tokio_tungstenite::tungstenite::{Message, Utf8Bytes};
@@ -6,21 +9,40 @@ use tokio_tungstenite::tungstenite::{Message, Utf8Bytes};
 /// The protocol defines the possible messages, sent from the clients or the server
 /// Some messages can be used for a request of something, some others are responding to another request
 /// We avoid naming it `Message` because there is already `tungstenite::Message`
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 pub enum Msg {
-    CreateSession { name: String, group_id: String },
-    DeleteSession {},  // the client_id will be used to verify the permission
+    StartSession {
+        name: String,
+        group_id: String,
+    },
+    StopSession {},    // the client_id will be used to verify the permission
     SessionStopped {}, // event to broadcast to all clients in the session
-    GetSessions { group_id: String },
-    JoinSession { name: String, group_id: String },
+    GetSessions {
+        group_id: String,
+    },
+    JoinSession {
+        name: String,
+        group_id: String,
+    },
     SessionJoined {}, // as a confirmation that JoinSession worked
     LeaveSession {},
     // Stats for leaders about how much followers have joined
-    Stats { followers: u32 },
+    Stats {
+        followers_count: u16,
+        leaders_count: u16,
+    },
 
     // Code exo syncing
-    SendCode { file: String, content: String },
-    SendResult { check_id: u32, passed: bool },
+    SendFile {
+        file: String,
+        content: String,
+    },
+    ForwardFile(ClientNum, ForwardedFile),
+    SendResult {
+        check_id: u32,
+        passed: bool,
+    },
+    ForwardResult(ClientNum, ForwardedResult),
 
     Error(LiveProtocolError),
 }
@@ -43,7 +65,7 @@ impl Msg {
 
 /// An error sent from the server to clients after any message
 /// that resolved in an error that is worth sending back to the client
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 pub enum LiveProtocolError {
     SessionNotFound,
     CannotJoinOtherSession,
@@ -60,3 +82,26 @@ impl Display for LiveProtocolError {
         f.write_str(text)
     }
 }
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct ForwardedFile {
+    /// The relative path inside the exo folder, like "main.cpp", "src/main.rs", "lib/image.h"
+    file: String,
+    content: String,
+    /// The time where this code was received on the server
+    time: std::time::SystemTime,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct ForwardedResult {
+    /// The relative path inside the exo folder, like "main.cpp", "src/main.rs", "lib/image.h"
+    pub check_id: u32,
+    pub passed: bool,
+    /// The time where this result was received on the server
+    pub time: std::time::SystemTime,
+}
+
+/// A incremental number attributed by the server to each client after session join, to let clients identify other clients.
+/// This MUST NOT be derived from the secret client_id, this ClientNum is not secret but should be different at each session.
+#[derive(Serialize, Deserialize, Clone)]
+pub struct ClientNum(pub u16);
