84747cdc987e54fcaf3ccc242d4d303b3d87186f
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2025-06-25 23:42:00 +0200

    feat(live): continue setup of session in ClientManager

--- src/live/client_manager.rs
+++ src/live/client_manager.rs
@@ -6,7 +6,7 @@ use super::{
     msg::Msg,
     session::{Session, SessionManager},
 };
-use futures_util::SinkExt;
+use futures_util::{stream::FusedStream, SinkExt};
 use log::{info, warn};
 use tokio::{
     select,
@@ -33,20 +33,24 @@ pub struct ClientManager {
 
 /// Keeping a link to the session via message passing in both directions
 pub struct SessionLink {
-    /// Allow to send a message to this client from the session manager
-    pub external_write_rx: UnboundedReceiver<Msg>,
-    /// A way to send Msg to the session manager if the message is authorized by the role
+    /// Allow to send a message from the session manager to this client
+    pub client_rx: UnboundedReceiver<Msg>,
+    /// A way to send messages to the session manager if the message is authorized by the role
     /// That's an Option because the client will exist before creating/joining a session
+    /// If it is None, it means the client hasn't joined a session yet
     pub session_tx: UnboundedSender<Msg>,
 }
 
 impl ClientManager {
     pub async fn run(&mut self) {
         loop {
+            if self.websocket.is_terminated() {
+                break;
+            }
             // We have to check if the session exist at each round,
             // because each message could have closed the session and set it to None
             match &mut self.session {
-                // Just listen on the websocket stream to wait for Get JoinSession message
+                // Just listen on the websocket stream to wait for GetSessions / JoinSession messages
                 None => {
                     let stream_el = self.websocket.next().await;
                     self.handle_msg(stream_el).await;
@@ -57,10 +61,10 @@ impl ClientManager {
                         stream_el = self.websocket.next() => {
                             self.handle_msg(stream_el).await;
                         }
-                        external_msg = session.external_write_rx.recv() => {
+                        external_msg = session.client_rx.recv() => {
                             if let Some(msg) = external_msg {
                                 let _ = self.websocket.send(msg.into_ws_msg().unwrap()).await;
-                                if let Msg::SessionStopped{} = msg {
+                                if let Msg::SessionStopped = msg {
                                     self.session = None;
                                 }
                             }
@@ -81,30 +85,54 @@ impl ClientManager {
                         // TODO: check name and group validity
                         // TODO: check name + group uniqueness
                         let session_info = Session { name, group_id };
-                        let (tx, rx) = tokio::sync::mpsc::unbounded_channel::<Msg>();
-                        let session_manager =
-                            SessionManager::new(session_info, self.client_id.clone(), rx);
+                        let (session_tx, session_rx) =
+                            tokio::sync::mpsc::unbounded_channel::<Msg>();
+                        let (client_tx, client_rx) = tokio::sync::mpsc::unbounded_channel::<Msg>();
+                        let mut session_manager = SessionManager::new(
+                            session_info,
+                            self.client_id.clone(),
+                            session_rx,
+                            client_tx,
+                        );
                         self.session = Some({
                             SessionLink {
-                                external_write_rx: todo!(),
-                                session_tx: tx,
+                                client_rx,
+                                session_tx,
                             }
                         });
+                        info!("Session created");
+                        self.role = ClientRole::Leader;
+                        let _ = self
+                            .websocket
+                            .send(Msg::SessionStarted.into_ws_msg().unwrap())
+                            .await;
+
+                        tokio::spawn(async move {
+                            session_manager.run().await;
+                        });
                     }
-                    Ok(Msg::StopSession {}) => {
+                    Ok(Msg::StopSession) => {
                         if self.role == ClientRole::Follower {
                             warn!("Got a DeleteSession with a follower role: {}", msg);
                             // that's a forged request, we can ignore it
                         }
-                        self.session = None;
+                        if let Some(session) = &self.session {
+                            let _ = session.session_tx.send(Msg::StopSession);
+                            self.role = ClientRole::Follower;
+                        }
+                        // Do not touch self.session for now, wait for the session_manager choosing
+                        // to stop itself via SessionStopped message
                     }
                     Ok(Msg::JoinSession { name, group_id }) => match &self.session {
                         Some(session) => {
-                            self.websocket.send(
-                                Msg::Error(LiveProtocolError::CannotJoinOtherSession)
-                                    .into_ws_msg()
-                                    .unwrap(),
-                            );
+                            let _ = self
+                                .websocket
+                                .send(
+                                    Msg::Error(LiveProtocolError::CannotJoinOtherSession)
+                                        .into_ws_msg()
+                                        .unwrap(),
+                                )
+                                .await;
                         }
                         None => {
                             todo!()
