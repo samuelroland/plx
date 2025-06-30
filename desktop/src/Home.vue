9aadad1faa9a9e5036ef81015fa99226b519d6b6
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2025-06-30 18:24:23 +0200

    feat(desktop): integrate TailwindCSS, add basic Home.vue with logo

---
+++ desktop/src/Home.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="p-2 sm:mx-5 md:mx-10 lg:mx-10 max-w-[1300px] flex items-center flex-col">
+        <img class="w-72 max-w-[80vw]" src="/logo.svg" />
+        <div>
+            <a target="_blank" href="https://github.com/samuelroland/plx">Git repository</a>
+        </div>
+        <!-- <h1 class="text-xl md:text-4xl my-5 nice-gradient">Practice programming in a deliberate Learning eXperience </h1> -->
+    </div>
+</template>
