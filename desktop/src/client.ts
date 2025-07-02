ab346f9e675f36ae16461cbadb4180b1b4aeabf2
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2025-07-02 22:55:07 +0200

    feat(desktop): start to build a TypeScript LiveClient
    I was using the Rust LiveClient in Tauri commands but when I started implementing
    streaming of a subset of events, it was just impossible to find a good pattern
    to have streaming of these events, in addition to continue to send
    Action on the socket. Managing states in Tauri and sending back to the frontend
    was also a pain to manage.

---
+++ desktop/src/client.ts
@@ -0,0 +1,94 @@
+import {
+  PROTOCOL_VERSION,
+  QUERYSTRING_LIVE_CLIENT_ID_FIELD,
+  QUERYSTRING_LIVE_PROTOCOL_VERSION_FIELD,
+} from "./ts/commands";
+import { Event, Action } from "./ts/bindings.ts";
+
+QUERYSTRING_LIVE_CLIENT_ID_FIELD;
+export class LiveClient {
+  socket: WebSocket | undefined;
+  private constructor(socket: WebSocket) {
+    this.socket = socket;
+  }
+
+  static connect(domain: string, port: number, client_id: string): LiveClient {
+    let querystring = new URLSearchParams();
+    querystring.append(QUERYSTRING_LIVE_CLIENT_ID_FIELD, client_id);
+    querystring.append(
+      QUERYSTRING_LIVE_PROTOCOL_VERSION_FIELD,
+      PROTOCOL_VERSION,
+    );
+    const socket = new WebSocket(
+      "ws://" + domain + ":" + port + "/?" + querystring.toString(),
+    );
+
+    socket.onmessage = (event: MessageEvent) => {
+      onMessage(event);
+    };
+    return new LiveClient(socket);
+  }
+
+  disconnect() {
+    this.socket?.close();
+  }
+}
+
+function onMessage(message: MessageEvent): any {
+  try {
+    let event = JSON.parse(message.data) as Event;
+    onEvent(event);
+  } catch {}
+}
+
+function onEvent(event: Event) {
+  console.log("Got event", event);
+  switch (event.type) {
+    case "SessionStarted":
+      break;
+    case "SessionStopped":
+      break;
+    case "SessionJoined":
+      break;
+    case "SessionsList":
+      break;
+    case "Stats":
+      break;
+    case "ServerStopped":
+      break;
+    case "ExoSwitched":
+      break;
+
+    case "ForwardFile": {
+      let entry = answers.value.get(event.content.client_num);
+      if (!entry)
+        entry = {
+          client_num: event.content.client_num,
+          checks_status: [],
+          files: new Map(),
+        };
+      entry.files.set(event.content.file.path, event.content.file);
+      answers.value.set(event.content.client_num, entry);
+      break;
+    }
+    case "ForwardResult":
+      let entry = answers.value.get(event.content.client_num);
+      if (!entry)
+        entry = {
+          client_num: event.content.client_num,
+          checks_status: [],
+          files: new Map(),
+        };
+      if (!entry)
+        entry = {
+          client_num: event.content.client_num,
+          checks_status: [],
+          files: new Map(),
+        };
+      entry.checks_status.push(event.content.result.check_result.state);
+      answers.value.set(event.content.client_num, entry);
+      break;
+    case "Stats":
+    case "ExoSwitched":
+  }
+}
