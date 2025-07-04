b0b6dd1a6ede96ca927f48826d6d6eb21d2351ac
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2025-07-04 11:08:38 +0200

    feat: show TreeSitter highlighted code in Code.vue, refresh via Tauri command

--- desktop/src/Code.vue
+++ desktop/src/Code.vue
@@ -1,11 +1,34 @@
 <script setup lang="ts">
+import { onMounted, ref, Ref, watch } from 'vue';
+import { commands } from './ts/commands';
 
-let props = defineProps<{ code: string }>()
+const highlightedCode: Ref<null | string> = ref(null)
 
+let props = defineProps<{ code: string, path: string }>()
+
+async function refreshCode(code: string) {
+    // trim code to avoid the empty lines at the end to take place
+    let result = await commands.highlightCodeWithTreeSitter(props.path, code.trim());
+    if (result.status == "ok") {
+        highlightedCode.value = result.data
+    }
+}
+
+// On each props.code change, if the code is different that before, refresh it
+watch(() => props.code, async (oldCode, newCode) => {
+    if (oldCode.trim() != newCode.trim()) {
+        refreshCode(newCode)
+    }
+})
+
+onMounted(() => {
+    refreshCode(props.code)
+})
 </script>
 
 <template>
-    <code>
-        <pre class="border border-orange-400 rounded-sm p-2">{{ props.code.trim() }}</pre>
-    </code>
+    <div class="relative">
+        <div class="absolute top-0 right-0 px-3 py-1 text-gray-600">{{ props.path }}</div>
+        <pre><code v-html="highlightedCode"></code></pre>
+    </div>
 </template>
