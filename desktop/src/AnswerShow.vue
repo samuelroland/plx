d163efbc73bdea003993b009ecfb1e8e22dbfb2f
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2025-07-04 11:09:18 +0200

    feat: show last file time in AnswerShow and use Code.vue

--- desktop/src/AnswerShow.vue
+++ desktop/src/AnswerShow.vue
@@ -1,17 +1,37 @@
 <script setup lang="ts">
 import { Answer } from './stores/LiveStore';
+import Code from './Code.vue';
+import { ForwardedFile } from './ts/bindings';
 
 let props = defineProps<{ answer: Answer }>()
+
+function lastFileTime(files: Map<string, ForwardedFile>) {
+    const maxTime = Math.max(...Array.from(files.values()).map(file => file.time))
+    return formatTimestampAsHoursMinutesSeconds(maxTime)
+}
+
+function formatTimestampAsHoursMinutesSeconds(time: number): string {
+    const date = new Date(time * 1000)
+    function normalize(number: number) {
+        let result = number.toString()
+        if (number < 10) {
+            result = "0" + number
+        }
+        return result
+    }
+    return normalize(date.getHours()) + ":" + normalize(date.getUTCMinutes()) + ":" + normalize(date.getSeconds())
+}
 
 </script>
 
 <template>
     <div>
-        <span>ClientNum {{ answer.client_num }}</span>
+        <div class="flex">
+            <div>{{ answer.client_num }} at {{ lastFileTime(answer.files) }}</div>
+        </div>
         <div v-for="file in answer.files.values()">
-            <div>
-                <span>{{ file.time }} - {{ file.path }}</span>
-                <Code :code="file.content"></Code>
+            <div class="mb-2">
+                <Code :key="file.path" :code="file.content" :path="file.path"></Code>
             </div>
         </div>
     </div>
