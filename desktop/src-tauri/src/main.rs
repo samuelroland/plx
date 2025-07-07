493da5d43af5da9306910e665cb09575a67fc110
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2025-07-07 21:49:57 +0200

    feat: enable loggin in desktop via plx-desktop-debug.log

--- desktop/src-tauri/src/main.rs
+++ desktop/src-tauri/src/main.rs
@@ -1,6 +1,18 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::fs::File;
+
+use simplelog::*;
+
 fn main() {
+    // Generate a simple debug file to be able to send logs on Windows where println! are not visible
+    WriteLogger::init(
+        LevelFilter::Trace,
+        Config::default(),
+        File::create("plx-desktop-debug.log").expect("Failed to create log file"),
+    )
+    .expect("Failed to initialize WriteLogger");
+
     plx_lib::run()
 }
