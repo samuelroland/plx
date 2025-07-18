db79d2155053750a68ce9af27e07785f8fa93db6
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2025-07-18 02:16:45 +0200

    refactor: delete examples/full as the new desktop app need a separated demo Git repos
    That's current github.com/samuelroland/plx-demo but this will change later.

--- examples/full/pointers/crash-debug-java/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-  public static void main(String[] args) {
-    Person p = null;
-    System.out.println("Hello there !");
-    System.out.println("My fullname is " + p.firstname + " " + p.lastname);
-  }
-}
--- examples/full/pointers/crash-debug-java/Main.sol.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-  public static void main(String[] args) {
-    Person p = new Person(args[0], args[1]);
-    System.out.println("Hello there !");
-    System.out.println("My fullname is " + p.firstname + " " + p.lastname);
-  }
-}
