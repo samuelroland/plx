9ec5e0965248e0aaca4358939fefae1f37949def
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2025-07-18 18:37:05 +0200

    feat(cli): make the subcommand mandatory and make the parse subcommand take a single path

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,24 +1,27 @@
 use clap::{Parser, Subcommand};
+use parse::parse_command;
 use plx_core::live::server::{DEFAULT_LIVE_PORT, LiveServer};
 use simplelog::*;
-use std::{fs::File, path::PathBuf};
+use std::{fs::File, path::PathBuf, process::exit};
+
+mod parse;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Cli {
     #[command(subcommand)]
-    command: Option<Commands>,
+    command: Commands,
 }
 
 #[derive(Subcommand)]
 enum Commands {
     /// Starts the live server
     Server,
-    /// Parse the given files or the entire course in the current folder otherwise
+    /// Parse the given file or folder or the entire course with the PLX DY spec format
     Parse {
-        #[arg(value_name = "FILE")]
-        /// The list of DY files to parse with the .dy extension
-        files: Option<Vec<PathBuf>>,
+        #[arg(value_name = "PATH")]
+        /// A PLX file with a .dy extension, or a folder with a course.dy
+        path: PathBuf,
     },
 }
 
@@ -33,17 +36,18 @@ fn main() {
     let cli = Cli::parse();
 
     // Start the server or the UI
-    match cli.command {
-        Some(Commands::Server) => {
+    let result = match cli.command {
+        Commands::Server => {
             let server = LiveServer::new().expect("Couldn't setup LiveServer...");
             println!("Started PLX server on port {DEFAULT_LIVE_PORT}");
             server.start(DEFAULT_LIVE_PORT, true); // this is blocking indefinitly
+            Ok(())
         }
-        Some(Commands::Parse { files }) => {
-            dbg!(files);
-        }
-        None => {
-            todo!()
-        }
+        Commands::Parse { path } => parse_command(path),
+    };
+
+    if let Err(e) = result {
+        eprintln!("Fatal error: {e}");
+        exit(2);
     }
 }
