49e6ec9d517a776827279ab98f9b5d4e88ff4f07
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2025-07-18 16:31:59 +0200

    feat: refactor CLI with clap to have a "server" subcommand and prepare a "parse" subcommand

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,6 +1,26 @@
+use clap::{Parser, Subcommand};
 use plx_core::live::server::{DEFAULT_LIVE_PORT, LiveServer};
 use simplelog::*;
-use std::{env::args, fs::File};
+use std::{fs::File, path::PathBuf};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Starts the live server
+    Server,
+    /// Parse the given files or the entire course in the current folder otherwise
+    Parse {
+        #[arg(value_name = "FILE")]
+        /// The list of DY files to parse with the .dy extension
+        files: Option<Vec<PathBuf>>,
+    },
+}
 
 fn main() {
     WriteLogger::init(
@@ -9,13 +29,21 @@ fn main() {
         File::create("debug.log").expect("Failed to create log file"),
     )
     .expect("Failed to initialize WriteLogger");
+
+    let cli = Cli::parse();
+
     // Start the server or the UI
-    let args: Vec<String> = args().collect();
-    if args.len() > 1 && args[1] == "server" {
-        let server = LiveServer::new().expect("Couldn't setup LiveServer...");
-        println!("Started PLX server on port {DEFAULT_LIVE_PORT}");
-        server.start(DEFAULT_LIVE_PORT, true); // this is blocking indefinitly
-    } else {
-        eprintln!("Only the server is available here, use 'server' argument");
+    match cli.command {
+        Some(Commands::Server) => {
+            let server = LiveServer::new().expect("Couldn't setup LiveServer...");
+            println!("Started PLX server on port {DEFAULT_LIVE_PORT}");
+            server.start(DEFAULT_LIVE_PORT, true); // this is blocking indefinitly
+        }
+        Some(Commands::Parse { files }) => {
+            dbg!(files);
+        }
+        None => {
+            todo!()
+        }
     }
 }
