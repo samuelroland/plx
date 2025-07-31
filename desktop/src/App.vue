fce860aef718f82c303f6be1e8e1cefb21ab72a5
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2025-07-31 23:54:11 +0200

    feat: make the top header blue when the client is connected to a server

--- desktop/src/App.vue
+++ desktop/src/App.vue
@@ -24,16 +24,21 @@ onMounted(async () => {
 <template>
     <div v-html="style"></div>
     <div class="w-full h-[100vh]">
-        <header class="h-7 bg-orange-100 flex px-3 p-1 items-center">
+        <header class="h-7 flex px-3 p-1 items-center" :class="live.isConnected ? 'bg-blue-200' : 'bg-orange-100'">
             <div class="flex-1 flex">
                 <img class="w-8 max-w-[80vw] inline" src="/logo.svg" />
+                <span :class="global.page == 'home' ? 'bg-blue-300' : ''"
+                    class="px-1 mx-2 cursor-pointer hover:font-bold" @click="global.page = 'home'">Home</span>
                 <span class="mx-2">-</span>
-                <span class="px-1 cursor-pointer hover:bg-orange-200" @click="global.page = 'course'">
+                <span :class="global.page == 'course' ? 'bg-blue-300' : ''" class="px-1 cursor-pointer hover:font-bold"
+                    @click="global.page = 'course'">
                     {{ train.course?.name }}
                 </span>
             </div>
-            <span v-if="live.session">Current session: {{ live.session.name }} - {{ live.role }} - number {{
-                live.client_num }} </span>
+            <span v-if="live.session">
+                - Current session: {{ live.session.name }}
+                - {{ live.role }}
+                - Client number {{ live.client_num }} </span>
             <span v-else class="text-gray-600/80 italic">No live session</span>
         </header>
         <div class="h-[95vh] flex items-center justify-center">
